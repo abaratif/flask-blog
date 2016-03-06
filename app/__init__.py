d6361483300870a0a532f0c68500eefe93d8d3b1
Arman B <arman.baratifar@utexas.edu>
2016-03-05 18:29:33 -0600

    Imported the config file in init

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
-
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
+
 from app import views
+
+# from flask import Flask
+
+# app = Flask(__name__)
+# from app import views
